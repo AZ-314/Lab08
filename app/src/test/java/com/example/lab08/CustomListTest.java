6198a8b82b3bd5a589fc6d8a2f42e5cc2066b265
Amina Zia <amina@Aminas-MacBook-Pro.local>
2026-03-17 23:04:48 +0500

    failing countCities test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,6 +3,16 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+
+    private City mockCity()
+    {
+        return new City("Lahore", "Punjab") ;
+    }
+    private CustomList mockCitylist(){
+        CustomList citylist = new CustomList() ;
+        citylist.addCity(mockCity()) ;
+        return citylist ;
+    }
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
@@ -12,5 +22,32 @@ public class CustomListTest {
         //exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testCountCities()
+    {
+        CustomList citylist = mockCitylist() ;
+        assertEquals(1, citylist.countCities());
+        City other = new City("Quetta", "Balochistan") ;
+        citylist.addCity(other) ;
+        assertEquals(2, citylist.countCities());
+    }
+    /*
+    @Test
+    void testDeleteCity()
+    {
+        CustomList citylist = mockCitylist() ;
+        City other = new City("Quetta", "Balochistan") ;
+        citylist.addCity(other) ;
+        assertEquals(2, citylist.getCities().size());
+        citylist.deleteCity(other);
+        assertEquals(1, citylist.getCities().size());
+        assertThrows(IllegalArgumentException.class, () -> {
+            citylist.deleteCity(other);
+        }) ;
+
+    }
+
+     */
 
 }
